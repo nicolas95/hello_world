4ecc1a658a478826885adbe49b196a8504193943
nicolas.scherer <nicolas.scherer@gateprotect.com>
2016-01-14 14:38:01 +0100

    Set up the Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWorld::Application.config.secret_key_base = 'b70dbd014ca11162ae19dfcc682bf49e04b3210ea0d473d0aa22ab51d7e2d4097d47995e9b2ffa1804f6ac68948c5a42fd07c8b3eeb92f1933135d6f045367b5'
